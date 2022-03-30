0c6a7ef43b3cda243d166a80d3e8cc564f8d8718
rinus30 <olekuirinus@gmail.com>
2022-03-30 15:29:45 +0700

    buat component left-sidebar

---
+++ resources/views/components/public-left-sidebar.blade.php
@@ -0,0 +1,152 @@
+<div class="page-head-blog">
+    <div class="single-blog-page">
+    <!-- search option start -->
+    <form action="#">
+        <div class="search-option">
+        <input type="text" placeholder="Search...">
+        <button class="button" type="submit">
+        <i class="bi bi-search"></i>
+        </button>
+        </div>
+    </form>
+    <!-- search option end -->
+    </div>
+    <div class="single-blog-page">
+    <!-- recent start -->
+        <div class="left-blog">
+            <h4>recent post</h4>
+            <div class="recent-post">
+            <!-- start single post -->
+                <div class="recent-single-post">
+                    <div class="post-img">
+                    <a href="#"><img src="assets/img/blog/1.jpg" alt=""></a>
+                    </div>
+                <div class="pst-content">
+                <p><a href="#"> Redug Lerse dolor sit amet consect adipis elit.</a></p>
+                </div>
+                </div>
+                <!-- End single post -->
+                <!-- start single post -->
+                <div class="recent-single-post">
+                    <div class="post-img">
+                    <a href="#">
+                        <img src="assets/img/blog/2.jpg" alt="">
+                    </a>
+                    </div>
+                    <div class="pst-content">
+                    <p><a href="#"> Redug Lerse dolor sit amet consect adipis elit.</a></p>
+                    </div>
+                </div>
+                <!-- End single post -->
+                <!-- start single post -->
+                <div class="recent-single-post">
+                    <div class="post-img">
+                    <a href="#">
+                        <img src="assets/img/blog/3.jpg" alt="">
+                    </a>
+                    </div>
+                    <div class="pst-content">
+                    <p><a href="#"> Redug Lerse dolor sit amet consect adipis elit.</a></p>
+                    </div>
+                </div>
+                <!-- End single post -->
+                <!-- start single post -->
+                <div class="recent-single-post">
+                    <div class="post-img">
+                    <a href="#">
+                        <img src="assets/img/blog/4.jpg" alt="">
+                    </a>
+                    </div>
+                    <div class="pst-content">
+                    <p><a href="#"> Redug Lerse dolor sit amet consect adipis elit.</a></p>
+                    </div>
+                </div>
+                <!-- End single post -->
+            </div>
+        </div>
+    <!-- recent end -->
+    </div>
+    <div class="single-blog-page">
+        <div class="left-blog">
+            <h4>categories</h4>
+            <ul>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Project</a>
+            </li>
+            <li>
+                <a href="#">Design</a>
+            </li>
+            <li>
+                <a href="#">wordpress</a>
+            </li>
+            <li>
+                <a href="#">Joomla</a>
+            </li>
+            <li>
+                <a href="#">Html</a>
+            </li>
+            <li>
+                <a href="#">Website</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+    <div class="single-blog-page">
+        <div class="left-blog">
+            <h4>archive</h4>
+            <ul>
+            <li>
+                <a href="#">07 July 2016</a>
+            </li>
+            <li>
+                <a href="#">29 June 2016</a>
+            </li>
+            <li>
+                <a href="#">13 May 2016</a>
+            </li>
+            <li>
+                <a href="#">20 March 2016</a>
+            </li>
+            <li>
+                <a href="#">09 Fabruary 2016</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+    <div class="single-blog-page">
+        <div class="left-tags blog-tags">
+            <div class="popular-tag left-side-tags left-blog">
+            <h4>popular tags</h4>
+                <ul>
+                    <li>
+                    <a href="#">Portfolio</a>
+                    </li>
+                    <li>
+                    <a href="#">Project</a>
+                    </li>
+                    <li>
+                    <a href="#">Design</a>
+                    </li>
+                    <li>
+                    <a href="#">wordpress</a>
+                    </li>
+                    <li>
+                    <a href="#">Joomla</a>
+                    </li>
+                    <li>
+                    <a href="#">Html</a>
+                    </li>
+                    <li>
+                    <a href="#">Masonry</a>
+                    </li>
+                    <li>
+                    <a href="#">Website</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
