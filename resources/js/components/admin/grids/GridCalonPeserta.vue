a8dc0d14680ba49460385ca92fa36c9b7dee875b
agriedd <dnr739528@gmail.com>
2022-04-05 13:04:37 +0800

    login public

---
+++ resources/js/components/admin/grids/GridCalonPeserta.vue
@@ -0,0 +1,42 @@
+<template>
+	<v-card flat color="indigo lighten-5" rounded="xl" link :to="{ name: 'peserta' }" style="min-height: 250px">
+		<v-card-text class="content-middle">
+			<div>
+				Total Calon Peserta
+			</div>
+			<v-slide-y-reverse-transition mode="out-in">
+				<div class="text-h3" :key="total">
+					{{ total | number }}
+				</div>
+			</v-slide-y-reverse-transition>
+		</v-card-text>
+	</v-card>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+	data(){
+		return {
+			total: 0
+		}
+	},
+	methods: {
+		...mapActions({
+			getItem: 'peserta/get',
+		}),
+		async loadItem(){
+			let res = await this.getItem({
+                status: 'calon',
+			}).catch(err => {
+				console.error(err)
+			})
+			if(res){
+				this.total = res.data?.meta?.total
+			}
+		},
+	},
+	created(){
+		this.loadItem()
+	},
+}
+</script>
---
+++ resources/js/components/admin/grids/GridPeserta.vue
@@ -0,0 +1,54 @@
+<template>
+	<v-card :color="'indigo lighten-4'" rounded="xl" flat link :to="{ name: 'peserta' }" style="min-height: 250px">
+		<div class="d-flex">
+			<v-card-text>
+				<div class="d-flex w-100">
+					<v-avatar :color="'indigo lighten-2'">
+						<v-icon>mdi-account</v-icon>
+					</v-avatar>
+					<v-spacer/>
+				</div>
+			</v-card-text>
+			<div class="grow">
+				<v-card-text class="text-h2 text-right">
+					<v-slide-y-reverse-transition mode="out-in">
+						<div class="text-h3" :key="total">
+							{{ total | number }}
+						</div>
+					</v-slide-y-reverse-transition>
+				</v-card-text>
+				<v-card-text class="text-right pt-0">
+					Total Peserta
+				</v-card-text>
+			</div>
+		</div>
+	</v-card>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+	data(){
+		return {
+			total: 0
+		}
+	},
+	methods: {
+		...mapActions({
+			getItem: 'peserta/get',
+		}),
+		async loadItem(){
+			let res = await this.getItem({
+                status: 'peserta'
+			}).catch(err => {
+				console.error(err)
+			})
+			if(res){
+				this.total = res.data?.meta?.total
+			}
+		},
+	},
+	created(){
+		this.loadItem()
+	},
+}
+</script>
