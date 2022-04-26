e60f938d189ee6dfbcfa473dfc5bd8749f385a54
rinus30 <olekuirinus@gmail.com>
2022-04-26 15:56:15 +0700

    ubah calon peserta dan peserta

--- resources/views/public/peserta.blade.php
+++ resources/views/public/peserta.blade.php
@@ -38,29 +38,11 @@
                     <!-- single-blog start -->
                     <article class="blog-post-wrapper">
                         <div class="post-thumbnail">
-                            <img src="{{ asset('assets/img/uptd/kepala-uptd.jpg') }}" alt="">
+                        <img src="assets/img/blog/6.jpg" alt="" />
                         </div>
                         <div class="post-information">
-                        <h2>{{ $post["title"] }}</h2>
-                            <div class="entry-meta">
-                                <span class="author-meta"><i class="bi bi-person"></i> <a href="#">admin</a></span>
-                                <span><i class="bi bi-clock"></i> march 28, 2016</span>
-                                <span class="tag-meta">
-                                    <i class="bi bi-folder"></i>
-                                    <a href="#">painting</a>,
-                                    <a href="#">work</a>
-                                </span>
-                                <span>
-                                    <i class="bi bi-tags"></i>
-                                    <a href="#">tools</a>,
-                                    <a href="#"> Humer</a>,
-                                    <a href="#">House</a>
-                                </span>
-                                <span><i class="bi bi-chat"></i> <a href="#">6 comments</a></span>
-                            </div>
-                            <div class="entry-content">
-                                <p>{{$post["body"]}}</p>
-                            </div>
+                        <h2>Daftar Peserta Pelatihan</h2>
+   
                         </div>
                     </article>
                     <div class="clear"></div>
