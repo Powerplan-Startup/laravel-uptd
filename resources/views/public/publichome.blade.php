d8d2e70b75e30fc55dbfded563ea130ea0f1b669
agriedd <dnr739528@gmail.com>
2022-05-10 17:32:46 +0800

    remove form message us

--- resources/views/public/publichome.blade.php
+++ resources/views/public/publichome.blade.php
@@ -123,7 +123,7 @@
                 <div class="row">
 
                     <!-- Start Google Map -->
-                    <div class="col-md-6">
+                    <div class="col-md-12">
                         <!-- Start Map --><iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3927.117109076514!2d123.61521808451924!3d-10.171135431926654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2c569cad5bf811b7%3A0x381cb66b36a37a05!2sUPTD.%20BLK%20Provinsi%20NTT!5e0!3m2!1sid!2sid!4v1648538849890!5m2!1sid!2sid"
                             width="100%" height="380" frameborder="0" style="border:0;" allowfullscreen></iframe>
@@ -131,34 +131,6 @@
                     </div>
                     <!-- End Google Map -->
 
-                    <!-- Start  contact -->
-                    <div class="col-md-6">
-                        <div class="form contact-form">
-                            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                                <div class="form-group">
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name"
-                                        required>
-                                </div>
-                                <div class="form-group mt-3">
-                                    <input type="email" class="form-control" name="email" id="email"
-                                        placeholder="Your Email" required>
-                                </div>
-                                <div class="form-group mt-3">
-                                    <input type="text" class="form-control" name="subject" id="subject"
-                                        placeholder="Subject" required>
-                                </div>
-                                <div class="form-group mt-3">
-                                    <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-                                </div>
-                                <div class="my-3">
-                                    <div class="loading">Loading</div>
-                                    <div class="error-message"></div>
-                                    <div class="sent-message">Your message has been sent. Thank you!</div>
-                                </div>
-                                <div class="text-center"><button type="submit">Send Message</button></div>
-                            </form>
-                        </div>
-                    </div>
                     <!-- End Left contact -->
                 </div>
             </div>
