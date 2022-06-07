dd361fe76c12e09410a7a38223478adf4f0d8a8e
November <a00t.wwp@gmail.com>
2022-06-07 20:28:16 +0800

    ok

--- database/migrations/2022_03_28_155420_create_jadwal_pelatihans_table.php
+++ database/migrations/2022_03_28_155420_create_jadwal_pelatihans_table.php
@@ -34,7 +34,7 @@ return new class extends Migration
                 ->references('nip')
                 ->on('instruktur')
                 ->onDelete('cascade')
-                ->onUpdate('cascade');
+                ->onUpdate('cascade');ph
 
             $table->timestamps();
         });
