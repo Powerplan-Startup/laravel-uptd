a76a75898c4c6f5392aa37a4e46fb4666ea291ad
agriedd <dnr739528@gmail.com>
2022-06-12 10:53:17 +0800

    jadwal ok

--- resources/js/pages/admin/instruktur/form/InputFotoInstruktur.vue
+++ resources/js/pages/admin/instruktur/form/InputFotoInstruktur.vue
@@ -28,7 +28,7 @@
                             </v-list-item-avatar>
                             <v-list-item-content>
                                 <v-list-item-title>
-                                    Pilih Berkas
+                                    Pilih Foto
                                 </v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
